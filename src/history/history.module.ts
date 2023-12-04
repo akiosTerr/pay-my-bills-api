beea1f9c196cb318dda56f84b95851b2e7339a8d
akiosTerr <stereoblindstudio@gmail.com>
2023-12-04 20:16:36 -0300

    Include docker support

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -4,9 +4,12 @@ import { RecurringBillSchema } from 'src/recurring-bills/schemas/recurring-bills
 import { HistoryController } from './history.controller';
 import { HistoryService } from './history.service';
 import { HistoryItemSchema } from './schemas/history-item';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: 'HistoryItem', schema: HistoryItemSchema},{name: 'RecurringBill', schema: RecurringBillSchema}])],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{name: 'HistoryItem', schema: HistoryItemSchema},{name: 'RecurringBill', schema: RecurringBillSchema}])],
   controllers: [HistoryController],
   providers: [HistoryService],
 })
