94d43c1ab645702d940173d7e43645d4e35a0a4e
akiosTerr <stereoblindstudio@gmail.com>
2024-02-13 19:42:30 -0300

    add serve static for react UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,17 @@ import { RecurringBillModule} from './recurring-bills/recurring-bills.module'
 import { MongooseModule } from '@nestjs/mongoose';
 import { HistoryModule } from './history/history.module';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'pay-my-bills', 'build'),
     }),
     MongooseModule.forRoot(process.env.DB_URI),
     RecurringBillModule, 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
+import express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // app.useStaticAssets(join(__dirname, '..', '..', 'pay-my-bills', 'build'));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
